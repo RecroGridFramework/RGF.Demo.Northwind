b492b26c3aac0eaca2e372369b36a5cfed9efb48
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-02-21 13:22:09 +0100

    ver 6.5

--- src/Entities/RG_Product.cs
+++ src/Entities/RG_Product.cs
@@ -3,45 +3,43 @@ using Recrovit.RecroGridFramework.Abstraction.Models;
 using Recrovit.RecroGridFramework.Extensions;
 using Recrovit.RecroGridFramework.Security;
 using RGF.Demo.Northwind.Models;
-using System;
-using System.Linq;
 
-namespace RGF.Demo.Northwind.Entities
+namespace RGF.Demo.Northwind.Entities;
+
+public partial class RG_Product : RecroGridDBC<Products>
 {
-    public partial class RG_Product : RecroGridDBC<Products>
+    public RG_Product(IRecroGridContext rgContext) : base(rgContext) { }
+
+    protected override async Task InitializeAsync(IRecroGridContext rgContext)
     {
-        public RG_Product(IRecroGridContext rgContext) : base(rgContext) { }
+        await base.InitializeAsync(rgContext);
 
-        protected override async Task InitializeAsync(IRecroGridContext rgContext)
+        RgfMenu menu = new();
+        menu.AddItem("Permission", "perm");
+        menu.AddItemGlobal("Permission:Custom", "custom");
+        RGOptions["RGO_CustomMenu"] = menu;
+    }
+
+    protected override Task<bool> OnCustomFunctionAsync(RecroTrack tracking, CustomFunctionEventArgs args, RGUIMessages messages)
+    {
+        var dataRec = args.DataRec as Products;
+        if (dataRec != null)
         {
-            await base.InitializeAsync(rgContext);
-
-            RgfMenu menu = new();
-            menu.AddItem("Permission", "perm");
-            menu.AddItemGlobal("Custom", "custom");
-            RGOptions["RGO_CustomMenu"] = menu;
-        }
-
-        protected override Task<(bool Success, Products DataRec)> OnCustomFunctionAsync(RecroTrack tracking, Products dataRec, CustomFunctionEventArgs args, RGUIMessages messages)
-        {
-            if (dataRec != null)
-            {
-                switch (args.FunctionName)
-                {
-                    case "perm":
-                        var permission = RecroSec.GetPermissionsForUser(tracking.UserId, "RG_Product", null, dataRec);
-                        messages.AddInfo(permission.CRUD);
-                        break;
-                }
-            }
             switch (args.FunctionName)
             {
-                case "custom":
-                    var permission1 = RecroSec.GetPermissionsForUser(tracking.UserId, "RG_Product", null, dataRec);
-                    messages.AddInfo(permission1.GetPermission("Custom") ? "yes" : "no");
+                case "perm":
+                    var permission = RecroSec.GetPermissionsForUser(tracking.UserId, "RG_Product", null, dataRec);
+                    messages.AddInfo(permission.CRUD);
                     break;
             }
-            return base.OnCustomFunctionAsync(tracking, dataRec!, args, messages);
         }
+        switch (args.FunctionName)
+        {
+            case "custom":
+                var permission1 = RecroSec.GetPermissionsForUser(tracking.UserId, "RG_Product", null, dataRec);
+                messages.AddInfo(permission1.GetPermission("Custom") ? "yes" : "no");
+                break;
+        }
+        return base.OnCustomFunctionAsync(tracking, args, messages);
     }
 }
